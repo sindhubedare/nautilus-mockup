3785a62db896b129d17838e5e6fe080f8bc34e9b
sindhubedare <sindhubedare1990@gmail.com>
2020-08-05 16:04:56 -0500

    added header

---
+++ src/Component/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// class Header extends React.Component{
+//     render(){
+//         return(
+//            <h1> hello</h1>
+//         );
+//     }
+// }
+function Header(){
+    return(
+            <div>
+                <h1 id="channel"> Channels</h1>
+                <h3 id="tuneIn" style={{textAlign:'center'}}>TUNE IN TO NAUTILUS DEEP DIVE PORTALS</h3>         
+            </div>    
+          );
+    }     
+
+
+export default Header;
